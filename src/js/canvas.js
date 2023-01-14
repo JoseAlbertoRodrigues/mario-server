0d3fd4376f25614d63f3a4782e9c47caa4acb99e
Jose Alberto <80777765+JoseAlbertoRodrigues@users.noreply.github.com>
2023-01-14 01:27:56 -0300

    adicionei algumas fotos para o jogo
    também coloquei o codigo js aqui

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,73 +1,180 @@
-import utils from './utils'
-
+import platform from '../img/platform.png'
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
 canvas.width = innerWidth
 canvas.height = innerHeight
 
-const mouse = {
-  x: innerWidth / 2,
-  y: innerHeight / 2
+const gravity = 0.5
+
+class Player {
+    constructor () {
+        this.position = {
+            x: 100,
+            y: 100
+        }
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+        this.width = 30
+        this.height = 30
+    }
+
+    draw() {
+        c.fillStyle ='red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+        if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+            this.velocity.y += gravity
+        } else {
+            this.velocity.y = 0
+        }
+    }
 }
 
-const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
+class Platform {
+    constructor({x, y}) {
+        this.position = {
+            x,
+            y
+        }
 
-// Event Listeners
-addEventListener('mousemove', (event) => {
-  mouse.x = event.clientX
-  mouse.y = event.clientY
-})
+        this.width = 200
+        this.height = 20
+    }
 
-addEventListener('resize', () => {
-  canvas.width = innerWidth
-  canvas.height = innerHeight
-
-  init()
-})
-
-// Objects
-class Object {
-  constructor(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
-  }
-
-  draw() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-    c.closePath()
-  }
-
-  update() {
-    this.draw()
-  }
+    draw() {
+        c.fillStyle = 'blue'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
 }
 
-// Implementation
-let objects
-function init() {
-  objects = []
+const player = new Player()
 
-  for (let i = 0; i < 400; i++) {
-    // objects.push()
-  }
+const platforms = [
+    new Platform({
+        x:200,
+        y:200
+    }),
+    new Platform({
+        x:600,
+        y:300
+    }),
+    new Platform({
+        x:1000,
+        y:400
+    })
+] // usar esse agora
+
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    }
 }
 
-// Animation Loop
+let scrollOffset = 0
+
 function animate() {
-  requestAnimationFrame(animate)
-  c.clearRect(0, 0, canvas.width, canvas.height)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    requestAnimationFrame(animate)
+    player.update()
+    platforms.forEach(platform => {
+        platform.draw()
+    })
 
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-  // objects.forEach(object => {
-  //  object.update()
-  // })
+    // key Pressed
+    if (keys.right.pressed && player.position.x < 400) {
+        player.velocity.x = 5
+    } else if (keys.left.pressed && player.position.x > 100) {
+        player.velocity.x = -5
+    } else {
+        player.velocity.x = 0
+
+        if(keys.right.pressed) {
+            scrollOffset += 5
+            platforms.forEach(platform => {
+                platform.position.x -= 5
+            })
+        } else if(keys.left.pressed) {
+            scrollOffset -= 5
+            platforms.forEach(platform => {
+                platform.position.x += 5
+            })
+        }
+    }
+
+    // platform collision detection
+    platforms.forEach(platform => {
+        if (player.position.y + player.height <= platform.position.y &&
+            player.position.y + player.height + player.velocity.y >= platform.position.y &&
+            player.position.x + player.width >= platform.position.x &&
+            player.position.x <= platform.position.x + platform.width) {
+            player.velocity.y = 0
+        }
+    })
+
+    // console.log(scrollOffset) testar se a rolagem passou de um determinado valor
+    if (scrollOffset > 2000) {
+        console.log('You Win! Parabêns!')
+    }
 }
 
-init()
 animate()
+
+addEventListener('keydown', ({key}) => {
+    // console.log(event.key)
+    switch (key) {
+        case 'a':
+            console.log('left')
+            keys.left.pressed = true
+            break
+        case 's':
+            console.log('down')
+            break
+        case 'd':
+            console.log('right')
+            keys.right.pressed = true
+            break
+        case 'w':
+            console.log('up')
+            // empurrar para cima sinal de -
+            player.velocity.y -= 10
+            break
+    }
+    console.log(keys.right.pressed)
+})
+
+// adicionado para o jogador parar de se mover quando pressionar uma tecla
+addEventListener('keyup', ({key}) => {
+    // console.log(event.key)
+    switch (key) {
+        case 'a':
+            console.log('left')
+            keys.left.pressed = false
+            break
+        case 's':
+            console.log('down')
+            break
+        case 'd':
+            console.log('right')
+            keys.right.pressed = false
+            // player.velocity.x = 0
+            break
+        case 'w':
+            console.log('up')
+            // empurrar para cima sinal de -
+            player.velocity.y -= 10
+            break
+    }
+    console.log(keys.right.pressed)
+})
